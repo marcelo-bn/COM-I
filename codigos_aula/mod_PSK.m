4aba0b28569385b1b3918efc5c494d1bf6323d5b
Marcelo Bittencourt <35068186+marcelo-bn@users.noreply.github.com>
2020-07-11 19:36:30 -0300

    Modulação digital

---
+++ codigos_aula/mod_PSK.m
@@ -0,0 +1,25 @@
+﻿clear all
+close all
+clc
+
+% Aula dia 23/06
+
+M = 4;
+SNR_Max = 15;
+
+info = randint(1,100000,M);
+%info_mod = exp(j*2*pi*info/M);
+info_mod = pskmod(info, M);
+
+for SNR = 0 : SNR_Max
+    info_rec = awgn(info_mod, SNR);
+    info_det = pskdemod(info_rec, M);
+    [num_err(SNR+1), SER(SNR+1)] = symerr(info, info_det);
+end
+
+semilogy([0:SNR_Max],SER)
+xlabel('SNR')
+ylabel('Taxa de Erro de Simbolo (SER)')
+title('Desempenho de erro de simbolo para o M-PSK')
+
+
---
+++ codigos_aula/modulacao-digital.m
@@ -0,0 +1,57 @@
+﻿clear all
+close all
+clc
+
+#* Parâmetros iniciais *
+M = 2; #Transmissão binária
+Fs = 50e3; #Frequência de amostragem
+Ts = 1/Fs; #Período de amostragem
+Rb = 2e3; #Taxa de transmissão
+Tb = 1/Rb; #Duração do bit
+N = Fs/Rb; #Fator de superamostragem. Neste script N tem que ser inteiro!
+A = 1; #Nível de amplitude do pulso
+fc = 8e3;
+filtro_tx = ones(1,N);
+
+#* Definição do eixo tempo *
+periodo_tx = 4; #Período de transmissão em segundos
+t = [0:Ts:periodo_tx-Ts];
+
+info_bin = randint(1,Rb*periodo_tx,M);
+info_up = upsample(info_bin, N);
+info_bb = filter(filtro_tx,1,info_up);
+phi_c = 0;
+phi_psk = (2*pi*info_bb)/M;
+phi_fsk = 0;
+phi_ask = 0;
+
+c_t = A*sin(2*pi*fc*t);
+sinal_psk = A*sin(2*pi*fc*t + phi_psk);
+sinal_fsk = A*sin(2*pi*fc*t.*(info_bb) + phi_fsk);
+sinal_ask = A*(info_bb).*sin(2*pi*fc*t + phi_ask);
+
+subplot(511)
+plot(t,info_bb)
+xlim([0 10*Tb])
+ylim([-2*A 2*A])
+title('Forma de onda transmitida')
+subplot(512)
+plot(t,c_t)
+xlim([0 10*Tb])
+ylim([-2*A 2*A])
+title('Sinal Portadora')
+subplot(513)
+plot(t, sinal_psk)
+xlim([0 10*Tb])
+ylim([-2*A 2*A])
+title('Modulacao PSK')
+subplot(514)
+plot(t, sinal_fsk)
+xlim([0 10*Tb])
+ylim([-2*A 2*A])
+title('Modulacao FSK')
+subplot(515)
+plot(t, sinal_ask)
+xlim([0 10*Tb])
+ylim([-2*A 2*A])
+title('Modulacao ASK')
